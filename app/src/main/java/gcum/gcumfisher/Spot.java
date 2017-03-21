fdff74f1aa5d7cbcd9bcfd70df2f1b6486e64cd5
glullien <gurvan.lullien@gmail.com>
2017-03-21 23:31:02 +0100

    Code cleaning

--- app/src/main/java/gcum/gcumfisher/Spot.java
+++ app/src/main/java/gcum/gcumfisher/Spot.java
@@ -1,12 +1,7 @@
 package gcum.gcumfisher;
 
 import android.content.res.Resources;
-import android.location.Address;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Spot {
     @NonNull
@@ -34,36 +29,9 @@ public class Spot {
         return result;
     }
 
-    static class FilteredResults {
-        @NonNull
-        final List<Spot> spots;
-        @Nullable
-        final String outOfParis;
-
-        FilteredResults(@NonNull List<Spot> spots, @Nullable String outOfParis) {
-            this.spots = spots;
-            this.outOfParis = outOfParis;
-        }
-
-        boolean isEmpty() {
-            return spots.isEmpty();
-        }
-    }
-
-    @NonNull
-    static FilteredResults filterParisSpots(@NonNull List<Address> addresses) {
-        final List<Spot> spots = new ArrayList<>(addresses.size());
-        String outOfParis = null;
-        for (Address address : addresses) {
-            String postalCode = address.getPostalCode();
-            String street = address.getThoroughfare();
-            if ((postalCode != null) && (street != null) && (postalCode.matches("\\d+"))) {
-                int district = Integer.parseInt(postalCode) - 75000;
-                if ((district < 1) || (district > 20)) outOfParis = street+" ("+postalCode+")";
-                else spots.add(new Spot(street, district));
-            }
-        }
-        return new FilteredResults(spots, spots.isEmpty()?outOfParis:null);
+    @Override
+    public String toString() {
+        return "Spot{street=" + street + ", district=" + district + "}";
     }
 
     @NonNull
