e75c5cb26d40f55611040a79261e7a12524af1b4
glullien <gurvan.lullien@gmail.com>
2017-03-28 16:26:36 +0200

    Min API 21

--- app/src/main/java/gcum/gcumfisher/connection/SSL.java
+++ app/src/main/java/gcum/gcumfisher/connection/SSL.java
@@ -13,7 +13,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
@@ -24,28 +23,23 @@ import gcum.gcumfisher.R;
 class SSL {
     @NonNull
     static SSLSocketFactory getSSLSocketFactory(@NonNull Resources resources, @NonNull String url) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, IOException, CertificateException {
-
-        CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        InputStream caInput = new BufferedInputStream(resources.openRawResource(url.contains("parisestunparking")?R.raw.certificate_peup:R.raw.certificate));
+        final CertificateFactory cf = CertificateFactory.getInstance("X.509");
 
         Certificate ca;
-        try {
+        try (InputStream caInput = new BufferedInputStream(resources.openRawResource(url.contains("parisestunparking") ? R.raw.certificate_peup : R.raw.certificate))) {
             ca = cf.generateCertificate(caInput);
-            System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
-        } finally {
-            caInput.close();
         }
 
-        String keyStoreType = KeyStore.getDefaultType();
+        final String keyStoreType = KeyStore.getDefaultType();
         KeyStore keyStore = KeyStore.getInstance(keyStoreType);
         keyStore.load(null, null);
         keyStore.setCertificateEntry("ca", ca);
 
-        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        final String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        final TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
         tmf.init(keyStore);
 
-        SSLContext context = SSLContext.getInstance("TLS");
+        final SSLContext context = SSLContext.getInstance("TLS");
         context.init(null, tmf.getTrustManagers(), null);
 
         return context.getSocketFactory();
